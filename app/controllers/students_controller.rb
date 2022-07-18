e749a035f22214de1b8066fb0e650862d2f7610c
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-07-18 21:27:08 +0300

    Completed

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,15 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show 
+    students = Student.find(params[:id])
+    render json: students
+  end
+
+  def studentspecific
+    # students = Student.find_by(params[:value1, :value2])
+    # render json: students
+    byebug
+  end
+
 end
